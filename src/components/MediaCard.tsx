71bdb6b8883a8cc495a660bf101d81548b7ab6b1
unknown <ksw22662266@gmail.com>
2025-03-05 17:48:28 +0900

    flip card styling2

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -6,12 +6,17 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function MediaCard() {
+interface MediaCardProps {
+    photoId: number;  // photoId prop 정의
+  }
+
+export default function MediaCard({ photoId }: MediaCardProps ) {
+    const x = photoId % 10;
   return (
     <Card sx={{ width: 150, height: 200 }}> 
       <CardMedia
         sx={{ height: 80 }}
-        image="/static/gh1.jpg"
+        image={`/static/gh${x}.jpg`}
         title="green iguana"
       />
       <CardContent>
