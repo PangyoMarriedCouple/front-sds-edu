d20c6ce7e08a20e56abea4f1a019a4b0e2bc5f1e
UjinLeee <lyj5341@naver.com>
2025-03-06 10:16:24 +0900

    New : GuestHouseResult.tsx, Modified: FlipCardPage.jsx, ResultPage.jsx

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,6 +1,6 @@
-// ResultPage.jsx
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import GuestHouseResult from '../components/GuestHouseResult';
 
 const ResultPage = () => {
   const location = useLocation();
@@ -10,11 +10,14 @@ const ResultPage = () => {
     <div>
       <h1>게임 결과</h1>
       {isCorrect ? (
-        <p>축하합니다! 정답을 맞추셨습니다.</p>
+        <p>🎉 축하합니다! 정답을 맞추셨습니다.</p>
       ) : (
-        <p>아쉽네요. 다음에 다시 도전해보세요.</p>
+        <p>😢 아쉽네요. 다음에 다시 도전해보세요.</p>
       )}
-      <p>걸린 시간: {elapsedTime}초</p>
+      <p>⏱️ 걸린 시간: {elapsedTime}초</p>
+
+      {/* 게스트하우스 정보를 보여주는 컴포넌트 */}
+      {guestHouseId && <GuestHouseResult guestHouseId={guestHouseId} />}
     </div>
   );
 };
